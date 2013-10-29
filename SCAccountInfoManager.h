6576ad21d0c7634f30bdd21370131bd7171abb06
Alessandro Sangiuliano <alex22_7@hotmail.com>
2013-10-29 00:40:27 +0000

    StepChat/SCAccountInfoManager.h: Added comments with documentation style
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Services/User/StepChat@9451 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- SCAccountInfoManager.h
+++ SCAccountInfoManager.h
@@ -18,10 +18,31 @@ License: Modified BSD
 	NSMutableString *gPath;
 }
 
+/**
+ * The SCAccountInfoManager is a class to handle user account information,
+ * like the JID. It also is used to store these informations.
+ */
+ 
+ /* This class is temporary. It was implemented to replace the AddressesKit
+  * code, that at the time did work bad on Linux and FreeBSD, with 
+  * not predictable behavior on 64 bit version of the two systems.
+  * (this comment will be removed soon) <- FIXME
+  */  
+ 
+
 @property (strong, nonatomic, readonly) NSString *filePath; 
 
+/**
+ * Read the JID from a file and returns it.
+ */
 - (NSString*) readJIDFromFileAtPath:(NSString*)aPath;
+/**
+ * Write a JID to a file at a given path
+ */
 - (void) writeJIDToFile:(JID*)aJID atPath:(NSString*)aPath;
+/**
+ * Compose a new JID from an old one
+ */
 - (NSString*) composeNewJIDWithOldJID:(JID*)oldJID withServer:(NSString*)aServer;
 
 @end
